d343a7b811393e54423e895c412caee9777d13fb
Braulio Alejandro Navarrete Horta <133619100+BraulioAlejandroNavarreteHorta@users.noreply.github.com>
2025-02-28 23:38:03 -0600

    [ADD] Best Selling Products Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import HeroSection from "@/components/HeroSection";
 import EspecializacionSection from "@/components/EspecializacionSection";
 import ProductosRecognized from "@/components/ProductosRecognized";
 import CategoriesSection from "@/components/CategoriesSection";
+import BestSellingProducts from "@/components/BestSellingProducts";
+
 export default function Home() {
   return (
     <main className="min-h-screen">
@@ -9,6 +11,7 @@ export default function Home() {
       <EspecializacionSection />
       <ProductosRecognized />
       <CategoriesSection />
+      <BestSellingProducts />
     </main>
   );
 }
