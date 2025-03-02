7c9d70abd8ee923aee15ca763ef23b2790906623
Braulio Alejandro Navarrete Horta <133619100+BraulioAlejandroNavarreteHorta@users.noreply.github.com>
2025-03-02 01:21:01 -0600

    [TEST] DELETE WHEN UPLOAD TO AMPLIFY

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,14 +5,14 @@ import { getDatabase } from "firebase/database";
 import { getAuth, browserLocalPersistence, setPersistence } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || 'AIzaSyBKFZ_siDDjbWly9OVgCWXHxiqlZtx_GVI',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || 'pelteria-central.firebaseapp.com',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || 'pelteria-central',
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || 'pelteria-central.firebasestorage.app',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '433482912791',
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '1:433482912791:web:9a1028800a28657e533852',
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || 'G-1753YJGTTW',
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL || 'https://pelteria-central-default-rtdb.firebaseio.com'
 };
 
 // Initialize Firebase
